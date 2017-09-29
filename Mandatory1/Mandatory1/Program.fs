803198456deb4e60740c4c2838d54a1586d9521d
Søren Mousten <s164436@student.dtu.dk>
2017-09-29 12:54:38 +0200

    Types and declerations

---
+++ Mandatory1/Mandatory1/Program.fs
@@ -0,0 +1,28 @@
+﻿
+
+type no = int                                           // tlf nr
+type yb = int                                           // year of birth
+type ths = string list                                  // themes of interests
+type name = string                                      // member name
+type mDescription = no * yb * ths                       // member description tuple
+type clubMember = (name * mDescription)                 // pair of member name and description      
+type register = clubMember list                         // list of clubmembers
+type arrang = bool                                      // 
+
+let reg = [("Tim",(42021021,1996,["soccer";"jazz"]));
+           ("Bob",(88888888,1974,["engineering";"explosives"]));
+           ("Scott",(12312831,1982,["tennis";"jazz"]))]
+
+let p1 = (112,1982,["soccer";"jazz"])
+let p2 = (113,1982,["soccer";"jazz"])
+
+let tlfno (no,_,_) = no
+let ybirth (_,yb,_) = yb
+let themes (_,_,ths) = ths
+
+
+
+let rec extractInterested p r = function
+    | (0,_) -> []
+    | (_,[]) -> []
+    | (p,r) -> 
